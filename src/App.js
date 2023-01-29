9a61d1180ea81c2bdba790f76322d927dfd28a82
trungtinvo <72297358+trungtinvo@users.noreply.github.com>
2023-01-28 21:04:52 -0800

    delete unused favicon

--- src/App.js
+++ src/App.js
@@ -7,12 +7,10 @@ import Home from "./components/container/Home/Home";
 import Skill from "./components/container/Skill/Skill";
 import Experience from "./components/container/Experience/Experience";
 import Navbar from "./components/Navbar/Navbar";
-import Favicon from "react-favicon";
 
 const App = () => {
   return (
     <div>
-      {/* <Favicon url={.} /> */}
       <Navbar />
       <Home />
       <About />
